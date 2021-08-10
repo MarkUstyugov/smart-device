4df03c19834246f50778652aa95075fe88edf4b1
Mark Ustyugov <mark.ustyugov@gmail.com>
2021-08-10 17:18:07 +0500

    удалил лишнее

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import {photoDescreption} from '.data.js';
-
-
-console.log(photoDescreption);
-// const photoDescreption = createPhotoDescription();
-// photoDescreption;
-// (getMaxCharLength('Some string', 140));
